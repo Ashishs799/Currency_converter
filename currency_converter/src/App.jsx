debde27f8c299fbb59e1fdc53e166db69f7e7f77
Ashishs799 <np03cs4s220007@heraldcollege.edu.np>
2024-06-30 21:10:27 +0545

    first commit

---
+++ currency_converter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Container } from "./components/Container";
+
+function App() {
+  return (
+    <>
+      <Container />
+    </>
+  );
+}
+
+export default App;
---
+++ currency_converter/src/components/Container.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Container.css";
+import { Select } from "./Select";
+export const Container = () => {
+  return (
+    <div className="main">
+      <div className="box">
+        <Select />
+      </div>
+    </div>
+  );
+};
